5520bb2c648ab22433922d3a66ad1d9c5b9d58fe
Your Name <joaovpinheiros@gmail.com>
2023-09-19 21:33:39 -0300

    barbeiro perfil

--- assest/js/navigation.js
+++ assest/js/navigation.js
@@ -1,5 +1,7 @@
 
-// menu configuration
+
+const navHome = document.getElementById('nav-Home');
+const screenHome = document.getElementById('pag-home');
 
 const iconMenu = document.getElementById('icon-menu')
 const listMenu = document.getElementById('list-menu')
@@ -15,5 +17,12 @@ iconMenu.addEventListener('click', () => {
     listMenu.style.display = 'none';
     visible = true;
   }
-})
+});
+
+
+navHome.addEventListener('click', () => {
+  console.log('asd')
+  jobsScreen.scrollIntoView({ behavior: 'smooth' });
+
+});
 
